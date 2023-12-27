7510dea48e4cf5ec3b6d3497f5e0584a7a8d370f
mapletoot <149334794+mapletoot@users.noreply.github.com>
2023-12-27 17:05:44 +0000

    Added pseudo code to pattern.py

---
+++ pattern.py
@@ -0,0 +1,5 @@
+# Pseudo code:
+# Initialize a string variable.
+# Create a for loop with 9 iterations.
+# If the iteration is one of the first 5, add a star to the string
+# Else, if the iteration is not in the first 5, remove a * from the string.
